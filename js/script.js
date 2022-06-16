9e75248319e91b4d735dfe65798fccedbf6a9079
shashank thallapelli <shashankthallapelli@shashanks-MacBook-Pro.local>
2022-06-16 23:24:09 +0530

    finised commits

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
